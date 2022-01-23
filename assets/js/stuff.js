d6705f23fc17c42c19753ea3192ea176ee66d826
Rodrigo <2201157@my.ipleiria.pt>
2022-01-23 00:31:12 +0000

    Inicio dos detalhes dos projetos

--- assets/js/stuff.js
+++ assets/js/stuff.js
@@ -33,5 +33,45 @@ window.open("https://wa.me/+351914594573", '_blank');
 }, false);
 
 
+function loadInfo(projeto){
 
+	var infoPanel = document.getElementById("info");
+
+	$.ajax({
+
+		type: 'GET',
+		url: 'assets/json/info.json',
+		contentType:"application/json; charset=utf-8",
+		dataType : 'json',
+		success : function(data){
+
+			let content;
+
+			for(let i = 0; i < data.length; i++){
+
+				if(projeto === data[i].titulo) content = data[i];
+	
+			}
+			
+			let htmlText = `<a href="#work"><img src="images/back_button.png" height="50px" width="50px"></a>
+									<div align="center"><h4>${content.titulo}</h4><p class="infoParah">${content.Descrição}</p></div>`;
+
+
+			infoPanel.innerHTML = htmlText;
+
+			for(let i = 0; i < content.Imagens.length; i++){
+
+				infoPanel.innerHTML += `<div class="infoMargin" align="center"><img class="infoPic" src="${content.Imagens[i].src}" ></div>`;
+
+			}
+
+		},
+		error : function(request, status,error){
+
+			alert(error);
+
+		}
+
+	});
+}
 
